c4b3b5bf14d57db60d3e9796c4f68a6036b468ec
bob <alexsamcabildo7@gmail.com>
2026-04-25 12:23:47 +0800

    Move CreateRouteRequest to its separate file out of CreateDriverRequest

--- Shared/Request/CreateDriverRequest.cs
+++ Shared/Request/CreateDriverRequest.cs
@@ -6,10 +6,3 @@
     public string ContactNumber { get; set; } = string.Empty;
     public DateTime DateHired { get; set; }
 }
-
-public class CreateRouteRequest
-{
-    public string RouteCode { get; set; }
-    public int StartLocation { get; set; }
-    public int EndLocation { get; set; }
-}
---
+++ Shared/Request/CreateRouteRequest.cs
@@ -0,0 +1,6 @@
+﻿public class CreateRouteRequest
+{
+    public string RouteCode { get; set; }
+    public int StartLocation { get; set; }
+    public int EndLocation { get; set; }
+}
