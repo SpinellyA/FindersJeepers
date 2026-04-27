80672e74305b8a800b7aff946d3d80442c327236
Alex <Alex@DESKTOP-SPROEVQ>
2026-04-27 17:24:48 +0800

    Add AssignJeepneysAsync to interface IDriverService

--- FindersJeepers/FindersJeepers/Application/Interfaces/IDriverService.cs
+++ FindersJeepers/FindersJeepers/Application/Interfaces/IDriverService.cs
@@ -6,4 +6,6 @@ public interface IDriverService
     Task DeleteAsync(int driverId);
     Task<List<GetDriverResponse>> GetAsync(int pageNumber = -1, int pageSize = -1);
     Task<GetDriverDetailResponse> GetByIdAsync(int driverId);
+
+    Task AssignJeepneysAsync(AssignJeepneysRequest request);
 }
