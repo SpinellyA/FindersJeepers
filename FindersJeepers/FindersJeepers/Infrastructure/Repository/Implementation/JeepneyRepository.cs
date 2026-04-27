0f4be9237fd4677363da50c0c10da408ee222619
Alex <Alex@DESKTOP-SPROEVQ>
2026-04-27 17:32:19 +0800

    Modify JeepneyRepository to include List<JeepneyDrivers> when fetching from db (Important for queries and transactions)

--- FindersJeepers/FindersJeepers/Infrastructure/Repository/Implementation/JeepneyRepository.cs
+++ FindersJeepers/FindersJeepers/Infrastructure/Repository/Implementation/JeepneyRepository.cs
@@ -6,4 +6,8 @@ public class JeepneyRepository : Repository<Jeepney>, IJeepneyRepository
     public JeepneyRepository(MyDbContext context) : base(context)
     {
     }
+    public override async Task<Jeepney> GetByIdAsync(int id)
+    {
+        return await _set.Include(x => x.Drivers).FirstOrDefaultAsync(x => x.Id == id);
+    }
 }
