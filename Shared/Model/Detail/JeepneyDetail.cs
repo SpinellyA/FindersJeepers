94c2cd196fd7c614451032e0fac012e17916bae8
bob <alexsamcabildo7@gmail.com>
2026-04-29 06:40:51 +0800

    Add new Dto for JeepneyDriver and replace have it DriverSummary in JeepneyDetail

--- Shared/Model/Detail/JeepneyDetail.cs
+++ Shared/Model/Detail/JeepneyDetail.cs
@@ -6,7 +6,7 @@
     public int Capacity { get; set; }
     public string RouteCode { get; set; } = string.Empty;
     public string CurrentStatus { get; set; } = string.Empty;
-    public List<DriverSummary> AssignedDrivers { get; set; } = new();
+    public List<JeepneyDriverDto> AssignedDrivers { get; set; } = new();
     public TripSummary? CurrentTrip { get; set; }
     public List<TripSummary> PastTrips { get; set; } = new();
 }
---
+++ Shared/Model/ListItem/JeepneyDriverDto.cs
@@ -0,0 +1,8 @@
+﻿public class JeepneyDriverDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime AssignedAt { get; set; }
+    public DateTime? UnassignedAt { get; set; }
+}
