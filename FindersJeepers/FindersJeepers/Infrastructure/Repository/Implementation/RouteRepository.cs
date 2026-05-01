48b488918e5819c350cda4a17837ce2140e0fa28
bob <alexsamcabildo7@gmail.com>
2026-05-02 05:30:00 +0800

    Override RouteRepository GetById to do eager loading

--- FindersJeepers/FindersJeepers/Infrastructure/Repository/Implementation/RouteRepository.cs
+++ FindersJeepers/FindersJeepers/Infrastructure/Repository/Implementation/RouteRepository.cs
@@ -1,6 +1,10 @@
+
+using Microsoft.EntityFrameworkCore;
+
 public class RouteRepository : Repository<Route>, IRouteRepository
 {
     public RouteRepository(MyDbContext context) : base(context)
     {
     }
+    public override async Task<Route> GetByIdAsync(int id) => await _set.Include(x => x.Stops).FirstOrDefaultAsync(x => x.Id == id);
 }
