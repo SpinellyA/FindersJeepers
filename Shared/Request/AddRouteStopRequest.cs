80efbe06caea8ec6853841e62b0f2af0192501b7
bob <alexsamcabildo7@gmail.com>
2026-05-02 05:32:33 +0800

    Add new request object for adding route stops.

---
+++ Shared/Request/AddRouteStopRequest.cs
@@ -0,0 +1,11 @@
+﻿public class AddRouteStopRequest
+{
+    public int RouteId { get; set; }
+    public List<LocationIndexPair> RouteStops { get; set; } // probably a bad idea to reuse routestopdto
+}
+
+public record LocationIndexPair
+{
+    public int LocationId { get; set; }
+    public int Index { get; set; }
+}
