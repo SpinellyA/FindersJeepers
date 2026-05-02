229431129dc6cdd6a394f625e0497a16e7b72c74
bob <alexsamcabildo7@gmail.com>
2026-05-02 21:20:22 +0800

    Add RouteDirection in AddRouteStopRequest

--- Shared/Request/AddRouteStopRequest.cs
+++ Shared/Request/AddRouteStopRequest.cs
@@ -2,10 +2,17 @@
 {
     public int RouteId { get; set; }
     public List<LocationIndexPair> RouteStops { get; set; } // probably a bad idea to reuse routestopdto
+    public RouteDirection RouteDirection { get; set; }
 }
 
 public record LocationIndexPair
 {
     public int LocationId { get; set; }
     public int Index { get; set; }
+}
+
+public enum RouteDirection
+{
+    Forward,
+    Return
 }
