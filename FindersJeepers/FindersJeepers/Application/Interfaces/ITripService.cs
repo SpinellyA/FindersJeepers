72f89edecc795d123c0fcccc19520220088e75de
bob <alexsamcabildo7@gmail.com>
2026-05-05 15:17:14 +0800

    Rename starttrip request object

--- FindersJeepers/FindersJeepers/Application/Interfaces/ITripService.cs
+++ FindersJeepers/FindersJeepers/Application/Interfaces/ITripService.cs
@@ -5,7 +5,7 @@ public interface ITripService
 {
     Task<List<TripDto>> GetTripsAsync();
     Task NextStop(NextStopRequest req);
-    Task CreateDriverTrip(StartDriverTripRequest req);
+    Task CreateDriverTrip(StartTripRequest req);
     Task<TripDetailDto> GetDetailAsync(int tripId);
     Task StartTrip(int tripId);
     Task CompleteTrip(int tripId);
