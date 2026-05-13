d82949fafe8746e0c2c5e13a53b0b9d660a6efde
Alex <Alex@DESKTOP-SPROEVQ>
2026-05-13 23:30:29 +0800

    Add new migrations due to domain entity change, which will also change the schema.

---
+++ FindersJeepers/FindersJeepers/Migrations/20260513104001_UpdateTripLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindersJeepers.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTripLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StopId",
+                table: "TripLogs",
+                newName: "LocationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LocationId",
+                table: "TripLogs",
+                newName: "StopId");
+        }
+    }
+}
---
+++ FindersJeepers/FindersJeepers/Migrations/20260513144528_UpdateTripLog2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FindersJeepers.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTripLog2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
