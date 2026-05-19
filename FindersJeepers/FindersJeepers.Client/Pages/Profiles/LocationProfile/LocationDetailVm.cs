2d047f0c639d4a5bb0b5942b69f894584e7fbbe3
bob <alexsamcabildo7@gmail.com>
2026-05-19 13:48:48 +0800

    Componentize LocationProfile.razor

---
+++ FindersJeepers/FindersJeepers.Client/Pages/Profiles/LocationProfile/LocationDetailVm.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Net.Http.Json;
+
+public class LocationDetailViewModel
+{
+    private readonly HttpClient _http;
+
+    public LocationDetail? Location { get; private set; }
+    public List<BreadcrumbItem> Breadcrumbs { get; private set; } = [];
+    public bool IsLoading => Location is null;
+
+    public LocationDetailViewModel(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task LoadAsync(int id)
+    {
+        Location = await _http.GetFromJsonAsync<LocationDetail>($"/api/v1/locations/{id}");
+
+        if (Location is not null)
+        {
+            Breadcrumbs =
+            [
+                new BreadcrumbItem("Locations", href: "/locations"),
+                new BreadcrumbItem(Location.Name, href: null, disabled: true)
+            ];
+        }
+    }
+}
